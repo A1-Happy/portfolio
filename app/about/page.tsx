239185a78e8ce2c1d73c15df1a1aba93fb0418c8
A1-Happy <hjangir2003@gmail.com>
2023-07-13 18:38:29 +0530

    new commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,9 +8,9 @@ export default function About() {
         <div className="font-bold text-2xl text-center mb-7">In regards to</div>
     </div>
         <Card className="p-4 2xl:mx-[400px] xl:mx-[300px] lg:mx-[200px] mb-8">
-        Hey, I'm Happy Jangir and I'm a computer science and engineering graduate with a passion for technology and a strong skill set. I have experience working both independently and in teams, and I thrive on collaborating and finding innovative solutions. My technical expertise includes programming languages like Java, C++, and Python, as well as web development technologies. 
+        Hey, I&apos;m Happy Jangir and I&apos;m a computer science and engineering graduate with a passion for technology and a strong skill set. I have experience working both independently and in teams, and I thrive on collaborating and finding innovative solutions. My technical expertise includes programming languages like Java, C++, and Python, as well as web development technologies. 
         <br /><br />
-        I have completed personal projects that have allowed me to apply my skills and gain real-world experience. I'm a problem solver with excellent analytical abilities and a commitment to staying up-to-date with the latest advancements in the field. I value clear communication and believe in fostering a collaborative and inclusive work environment. I'm excited to contribute my skills and knowledge to a forward-thinking organization. Thank you for visiting my portfolio, and I look forward to connecting with you.
+        I have completed personal projects that have allowed me to apply my skills and gain real-world experience. I&apos;m a problem solver with excellent analytical abilities and a commitment to staying up-to-date with the latest advancements in the field. I value clear communication and believe in fostering a collaborative and inclusive work environment. I&apos;m excited to contribute my skills and knowledge to a forward-thinking organization. Thank you for visiting my portfolio, and I look forward to connecting with you.
         </Card>
 
         <span><ALink link="/Resume.pdf" target="_blank">↓ Resume</ALink></span>
